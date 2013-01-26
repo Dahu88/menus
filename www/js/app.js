6bcd16cb9ddf2ac5be41927676bc6b14ecdc8860
Sandrine Soudant <Sandrine.Soudant@gmail.com>
2013-01-26 14:50:58 +0100

    Ouvre la database et enregistre les données

--- www/js/app.js
+++ www/js/app.js
@@ -13,13 +13,75 @@ define(function(require) {
     require('layouts/layouts');
 
     // Write your app here.
+const DB_NAME = "MenuListDatabase";
+const DB_VERSION = 0;
+const lunchDatas = [
+	{ id: "", day: "", meal: "", description: "" }
+];
 
+    function openDB() {
+	alert(window.indexedDB);
+	console.log("openDb ...");
+	//var request = window.indexedDB.open(DB_NAME, DB_VERSION);
+	var request = window.indexedDB.open('MenuListDatabase', DB_VERSION);
+	request.onerror = function(event) {
+		alert("Why didn't you allow my web app to use IndexedDB?!\nDatabase error: " + event.target.errorCode);
+	};
+	request.onsuccess = function(event) {
+		db = request.result;
+	};
+	request.onupgradeneeded = function(event) {
+		// Update object stores and indices ....
+	};
+
+	// Create an objectStore to hold information about our customers. We're
+	// going to use "ssn" as our key path because it's guaranteed to be
+	// unique.
+	var objectStore = db.createObjectStore(DB_NAME, { keyPath: "id" });
+ 
+	// Create an index to search customers by name. We may have duplicates
+	// so we can't use a unique index.
+	objectStore.createIndex("day", "day", { unique: false });
+
+	// Create an index to search customers by email. We want to ensure that
+	// no two customers have the same email, so use a unique index.
+	objectStore.createIndex("id", "id", { unique: true });
+	
+	var transaction = db.transaction([DB_NAME], "readwrite");
+	transaction.oncomplete = function(event) {
+		alert("All done!");
+	};
+ 
+	transaction.onerror = function(event) {
+		// FIXME : Don't forget to handle errors!
+	};
+ 
+    }
+
+    function store(id, day, meal, description) {
+	// TODO : récupérer les données de l'IHM
+	// Store values in the newly created objectStore.
+	var lunch = {
+		"id": id,
+		"day": day,
+		"meal": meal,
+		"description": description
+	};
+	objectStore.add(lunch);
+	request.onsuccess = function(event) {
+    		event.target.result == lunch.id
+		// TODO : il faudrait peut-être exploiter ça
+		console.log("on vient de stocker dans la DB: " + lunch.id);
+	};
+    }
 
     function formatDate(d) {
         return (d.getMonth()+1) + '/' +
             d.getDate() + '/' +
             d.getFullYear();
     }
+
+    openDB();
 
     // List view
 
@@ -85,7 +147,7 @@ define(function(require) {
                        plat: plat.val(),
                        desc: desc.val() });
         }
-
+	storeDB(el.id, jour, plat, desc);
         edit.close();
     });
 });
